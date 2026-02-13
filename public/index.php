7f0a97a1cfe738a9f6dbdf8fede979419964b0e6
luis93md <luisportgas93@gmail.com>
2026-02-13 16:18:40 -0500

    descomentar de index

--- public/index.php
+++ public/index.php
@@ -7,14 +7,14 @@ error_reporting(E_ALL);
 require_once __DIR__ . '/../config/init.php';
 require_once __DIR__ . '/../config/Container.php';
 require_once __DIR__ . '/../config/Services/ContainerConfigurator.php';
-#require_once __DIR__ . '/../Routes/UsersEndPoints/userRoutes.php';
+#
 
 use Infrastructure\Middlewares\CorsMiddleware;
 
 // Ejecutar CORS primero
 $cors = new CorsMiddleware();
 $cors->handle();
-
-require_once __DIR__ . '/../Routes/loadRoutes.php';
+require_once __DIR__ . '/../Routes/UsersEndPoints/userRoutes.php';
+#require_once __DIR__ . '/../Routes/loadRoutes.php';
 
 
