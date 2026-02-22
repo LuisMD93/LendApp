04e2d77524907da80a71fd10ad4220c0266fef70
luis93md <luisportgas93@gmail.com>
2026-02-21 23:17:34 -0500

    trabajando modulu addUser test10

--- src/Application/UsersUseCases/AddUsers.php
+++ src/Application/UsersUseCases/AddUsers.php
@@ -19,10 +19,14 @@ class AddUsers {
 
             $response = $this->userRepository->existsUser($userDto->getEmail(),$userDto->getPhone());
             if ($response==false){
+                echo '<pre>';print_r(["respuesta"=>$response]);echo '</pre>';
                  $userEntity = UserMapper::toEntity($userDto);
+                echo '<pre>';print_r(["entidadMapeada"=>$userEntity]);echo '</pre>';
                  $response = $this->userRepository->createUser($userEntity);
+                  echo '<pre>';print_r(["responseCreate"=>$response]);echo '</pre>';
+                 echo '<pre>';print_r(["response"=>"No existe en bd"]);echo '</pre>';
                  return $response;  
-               echo '<pre>';print_r(["response"=>"No existe en bd"]);echo '</pre>';die;
+              
             }else{
                 echo '<pre>';print_r(["response"=>"Existe en bd" ,"Email"=>$userDto->getEmail(),"Phone"=>$userDto->getPhone()]);echo '</pre>';die;
             }
