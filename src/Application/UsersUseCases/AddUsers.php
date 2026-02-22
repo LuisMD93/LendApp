b1dba8faaa3a39abbd4c17f03fe10288d501d5d4
luis93md <luisportgas93@gmail.com>
2026-02-21 22:56:45 -0500

    trabajando modulu addUser test8

--- src/Application/UsersUseCases/AddUsers.php
+++ src/Application/UsersUseCases/AddUsers.php
@@ -17,16 +17,16 @@ class AddUsers {
 
     public function execute(UserDto $userDto): bool {
 
-            // $response = $this->userRepository->existsUser($userDto->getEmail(),$userDto->getPhone());
-            // if ($response==false) {Debe retornar falso
-            //    echo '<pre>';print_r(["response"=>"No existe en bd"]);echo '</pre>';die;
-            // }else{
-            //     echo '<pre>';print_r(["response"=>"Existe en bd"]);echo '</pre>';die;
-            // }
-            
-            if($this->userRepository->existsUser($userDto->getEmail(),$userDto->getPhone())){
-               return false;
+            $response = $this->userRepository->existsUser($userDto->getEmail(),$userDto->getPhone());
+            if ($response==false){
+               echo '<pre>';print_r(["response"=>"No existe en bd"]);echo '</pre>';die;
+            }else{
+                echo '<pre>';print_r(["response"=>"Existe en bd" ,"Email"=>$userDto->getEmail(),"Phone"=>$userDto->getPhone()]);echo '</pre>';die;
             }
+            
+            // if($this->userRepository->existsUser($userDto->getEmail(),$userDto->getPhone())){
+            //    return false;
+            // }
             $userEntity = UserMapper::toEntity($userDto);
             $response = $this->userRepository->createUser($userEntity);
             return $response;  
