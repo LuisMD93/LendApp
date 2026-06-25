332be0d34c9bd11a423194edd9e3649dc3b8185c
luimd <luisportgas93@gmail.com>
2026-06-25 00:16:01 -0500

    validando parametro id pago test3

--- src/Infrastructure/Http/ReportEndPoints/reportRoutes.php
+++ src/Infrastructure/Http/ReportEndPoints/reportRoutes.php
@@ -129,7 +129,7 @@ switch ($router->method) {
 
                     $isExperation = $auth->ValidateTokenExpiration($headers);
                     if($isExperation){
-                       $queryParam = implode(",",$headers['param']) || '';
+                       $queryParam = implode(",",$headers['param']) || 'ssss';
                        $controller->changeStatus($queryParam);
                     }else{
                         Response::error(false,Constans::ERROR_MESSAGE_TOKEN,401);
