7dcd9cded1d1176f4c4b0619ccbef85db9c79f23
luimd <luisportgas93@gmail.com>
2026-07-04 01:06:47 -0500

    Probando procedure add_report2

--- src/Infrastructure/Persistence/Repository/ReportRepository.php
+++ src/Infrastructure/Persistence/Repository/ReportRepository.php
@@ -51,18 +51,7 @@ class ReportRepository implements IReportRepository  {
                 $id_user = $report->getIdUser();
 
                 $creationDate = $report->getCreationDate()->format('Y-m-d H:i:s');
-                $modificationDate = $report->getModificationDate()->format('Y-m-d H:i:s');
-                echo json_encode([
-                    'loan_location' => $loan_location,
-                    'productName' => $productName,
-                    'amount' => $amount,
-                    'lendStatus' => $lendStatus,
-                    'description' => $description,
-                    'id_user' => $id_user,
-                    'creationDate' => $creationDate,
-                    'modificationDate' => $modificationDate
-                ], JSON_PRETTY_PRINT);
-             
+                $modificationDate = $report->getModificationDate()->format('Y-m-d H:i:s');             
 
                 $stmt->bindParam(':p_location_name_', $loan_location, PDO::PARAM_STR);
                 $stmt->bindParam(':p_product_Name_', $productName, PDO::PARAM_STR);
