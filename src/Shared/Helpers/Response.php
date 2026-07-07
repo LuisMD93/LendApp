e6f660321e08bcfbc19cd48e743e87dc7ae24e21
luimd <luisportgas93@gmail.com>
2026-07-07 02:06:08 -0500

    Haciendo relacion entre cliente y reporte test10

--- src/Shared/Helpers/Response.php
+++ src/Shared/Helpers/Response.php
@@ -18,7 +18,7 @@ class Response{
    static function arrayParse($request) : array {
         
         $data = json_decode(file_get_contents($request), true); // Obtener datos JSON del cuerpo de la solicitud
-        echo '<pre>';print($data);echo'</pre>';die;
+        echo '<pre>';print($request);echo'</pre>';die;
        // Response::If_startDate_Is_Empty($data);// si trabajas con fechas y  quieres ajustarlo a la fecha local
               echo json_encode([
                     "origin" => "Response",
