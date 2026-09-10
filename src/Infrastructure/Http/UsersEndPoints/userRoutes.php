d174df152578c0dcbf98c0da6eb61b551d726f67
luimd <luisportgas93@gmail.com>
2026-09-10 12:49:13 -0500

    todo OK

--- src/Infrastructure/Http/UsersEndPoints/userRoutes.php
+++ src/Infrastructure/Http/UsersEndPoints/userRoutes.php
@@ -131,9 +131,7 @@ switch ($router->method) {
                        
                 break;
             case 'user/login':
-                echo '<pre>';
-                   var_dump($headers);
-                echo' </pre>';exit;
+            
                 try {
                       return Response::success(
                                                 true,
